13126398842433005b3b99171b52d58f83490062
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2016-06-21 22:58:24 -0400

    Begin implementing a way for a list to be associated with an entity.

--- KlaviyoAdapter.php
+++ KlaviyoAdapter.php
@@ -34,6 +34,69 @@ class KlaviyoAdapter {
     return $this->siteId;
   }
 
+
+  public function getLists() {
+    $lists = array();
+
+    try {
+      $lists = $this->api->service('lists')->getAllLists();
+    }
+    catch (Exception $e) {
+      watchdog_exception($e);
+      drupal_set_message('Unable to retrieve lists from Klaviyo. Please try again later.', 'error');
+    }
+
+    return $lists;
+  }
+
+  public function createList($list_name) {
+    $list = NULL;
+    try {
+      $list = $this->api->service('lists')->createList($list_name);
+
+      cache_clear_all('klaviyo:list_options', 'cache');
+    }
+    catch (Exception $e) {
+      watchdog_exception('klaviyo', $e);
+      drupal_set_message('Unable create list. Please try again later.', 'error');
+    }
+
+    return $list;
+  }
+
+  public function getCachedListOptions() {
+    $cache = cache_get('klaviyo:list_options');
+
+    $options = array();
+    if ($cache && !empty($cache->data)) {
+      $options = $cache->data;
+    }
+    else {
+      $lists = KlaviyoAdapter::getInstance()->getLists();
+      foreach ($lists as $list) {
+        $options[$list->listType . ':' . $list->id] = check_plain($list->name);
+      }
+      cache_set('klaviyo:list_options', $options, 'cache', CACHE_TEMPORARY);
+    }
+
+    return $options;
+  }
+
+  public function createFullListId($list) {
+    return $list->listType . ':' . $list->id;
+  }
+
+  public function parseFullListId($list_full_id) {
+    $list_options = $this->getCachedListOptions();
+
+    $parsed_list_full_id = array();
+    if (isset($list_options[$list_full_id])) {
+      $parsed_list_full_id = explode(':', $list_full_id);
+    }
+
+    return $parsed_list_full_id + array(0 => '', 1 => '');
+  }
+
   public function getPersonGetMappableKeys() {
     $model_class = $this->api->getModelClass('person');
     return array_filter(call_user_func("$model_class::getAttributeKeys"), function($attribute_key) {
@@ -45,14 +108,12 @@ class KlaviyoAdapter {
     $person_configuration = array();
 
     $wrapper = entity_metadata_wrapper($entity_type, $entity);
-
     $this->preparePersonConfigurationKlaviyoAttributes($entity_type, $wrapper, $person_configuration);
-    if (!empty($person_configuration)) {
-      $person_configuration = $this->preparePersonConfigurationDrupalInfo($entity_type, $wrapper, $person_configuration);
 
-      if ($entity_type === 'user') {
-        $person_configuration = $this->preparePersonConfigurationFromAccount($wrapper, $person_configuration);
-      }
+    $person_configuration = $this->preparePersonConfigurationDrupalInfo($entity_type, $wrapper, $person_configuration);
+
+    if ($entity_type === 'user') {
+      $person_configuration = $this->preparePersonConfigurationFromAccount($wrapper, $person_configuration);
     }
 
     return $person_configuration;
@@ -62,8 +123,8 @@ class KlaviyoAdapter {
     $attributes = variable_get('klaviyo_person_attributes_' . $entity_type, array());
 
     foreach ($attributes as $field_name => $attribute_key) {
-      if (isset($wrapper->{$field_name}) && $field_value = $wrapper->{$field_name}->value()) {
-        $person_configuration[$attribute_key] = $field_value;
+      if (isset($wrapper->{$field_name})) {
+        $person_configuration[$attribute_key] = $field_value = $wrapper->{$field_name}->value();
       }
     }
 
@@ -90,21 +151,56 @@ class KlaviyoAdapter {
     return $person_configuration;
   }
 
-  public function isFieldMappable($entity_type, $bundle, $field_type) {
+  public function isFieldMappable($entity_type, $field_type, $bundle = '') {
     $allowable_field_types = array('text', 'list_text');
 
-    return ($entity_type === 'user' && $bundle === 'user' && in_array($field_type, $allowable_field_types));
+    return ($this->isEnabledOnEntity($entity_type, $bundle) && in_array($field_type, $allowable_field_types));
   }
 
-  public function isFieldMapped($entity_type, $bundle, $field_type) {
-    $is_mapped = FALSE;
+  public function getAllEntitySettings($entity_type, $bundle = '') {
+    $variable_name = "klaviyo_entity_settings_$entity_type";
 
-    if ($entity_type === 'user' && $bundle === 'user') {
-      $attributes = variable_get('klaviyo_person_attributes_user', array());
+    if ($bundle) {
+      $variable_name .= $bundle;
+    }
+
+    return variable_get($variable_name, array('enabled' => FALSE, 'list' => ''));
+  }
+
+  public function isEnabledOnEntity($entity_type, $bundle = '') {
+    $settings = $this->getAllEntitySettings($entity_type, $bundle);
+    return $this->isCompatiableEntity($entity_type, $bundle) && !empty($settings['enabled']);
+  }
+
+  public function isCompatiableEntity($entity_type, $bundle = '') {
+    return $entity_type === 'user';
+  }
+
+  public function isFieldMapped($entity_type, $field_type, $bundle = '') {
+    $is_mapped = FALSE;
+    $variable_name = "klaviyo_person_attributes__$entity_type";
+
+    if ($bundle) {
+      $variable_name .= $bundle;
+    }
+
+    if ($entity_type === 'user') {
+      $attributes = variable_get($variable_name, array());
       $is_mapped = !empty($attributes[$field_name]);
     }
 
     return $is_mapped;
+  }
+
+  public function subscribePersonToList(PersonModel $person, ListModel $list) {
+    if (!empty($list)) {
+      try {
+        $this->api->service('lists')->addPersonToList($person, $list);
+      }
+      catch (Exception $e) {
+        watchdog_error('klaviyo', $e);
+      }
+    }
   }
 
 
